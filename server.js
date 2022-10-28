de3086da64646d7cf714949eb9569d7010e5d63b
UpgradeOfficial <odeyemiincrease@yahoo.com>
2022-10-28 13:37:15 +0100

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
+var cors = require('cors')
 const PORT = process.env.PORT || 3500;
 const app = express();
-
+app.use(cors());
 app.get("/", (req, res) => {
   const data = {
     slackUsername: "Incre",
